cee3d60b56599bf592219a806224af2652b1f8c8
devohda <fffvvv6@gmail.com>
2021-09-08 00:22:01 +0900

    [PROGRAMMERS] 12946 : 하노이의 탑

--- devohda/python/PROGRAMMERS 12946.py
+++ devohda/python/PROGRAMMERS 12946.py
@@ -1,24 +1,17 @@
-from collections import deque
-
-
-def hanoi_tower(start, sub, target, height, answer):
+def hanoi_tower(start, sub, target, height, move):
     # 하나 작은 탑을 sub 에 만든다.
-    # 마지막 남은 거 하나를 타겟에 옮긴다.
-    # 다시 하나 작은 탑을 target 에 만든다.
+    # 마지막 남은 거 하나를 target에 옮긴다.
+    # 다시 하나 작은 탑을 target에 만든다.
 
     if height == 0:
         return
 
-    hanoi_tower(start, target, sub, height - 1, answer)
-    answer.append([start, target])
-    hanoi_tower(sub, start, target, height - 1, answer)
+    hanoi_tower(start, target, sub, height - 1, move)
+    move.append([start, target])
+    hanoi_tower(sub, start, target, height - 1, move)
 
 
 def solution(n):
     answer = []
     hanoi_tower(1, 2, 3, n, answer)
     return answer
-
-
-tower_height = int(input())
-print(solution(tower_height))
